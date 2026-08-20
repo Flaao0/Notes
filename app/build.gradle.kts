307929488ad6b83c36f2a377418eb018cead79ef
Flaao0 <v.petunin06@mail.ru>
2026-08-20 13:13:11 +0300

    feat: add view model dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
